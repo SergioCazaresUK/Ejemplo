b5c080e5b3adeee7c1f97279f4bcbc36ee3c1189
Sergio Armando Cazares Ruiz <Cazares250@outlook.com>
2022-05-14 11:52:05 -0700

    Concatenar

--- Nombre/Program.cs
+++ Nombre/Program.cs
@@ -8,8 +8,21 @@
 //    Console.WriteLine(nombre);
 //}
 
-int numero = 9;
-string numeroConCero = "99";
-numeroConCero = numeroConCero.PadLeft(3, '$');
-Console.WriteLine(numeroConCero);
+//int numero = 9;
+//string numeroConCero = "99";
+//numeroConCero = numeroConCero.PadLeft(3, '$');
+//Console.WriteLine(numeroConCero);
+
+string nombre = "Sergio";
+string apellido = "Cazares";
+int edad = 18;
+
+//string nombreYedad = nombre + "|" + apellido + "|" + edad;
+//Console.WriteLine(nombreYedad);
+
+//string nombreYedad = string.Format("{0}|{1}|{2}", nombre, apellido, edad);
+//Console.WriteLine(nombreYedad);
+
+string nombreYedad = $"{nombre}|{apellido}|{edad}";
+Console.WriteLine(nombreYedad);
 
