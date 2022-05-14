75fa554d7ad45dc471319ac86e5126a367c43aa8
Sergio Armando Cazares Ruiz <Cazares250@outlook.com>
2022-05-14 11:39:25 -0700

    PadLeft, WhiteSpace

--- Nombre/Program.cs
+++ Nombre/Program.cs
@@ -1,9 +1,15 @@
-﻿string nombre = "Coca";
-if (string.IsNullOrWhiteSpace(nombre)) 
-{
-    Console.WriteLine("string vacio");
-}
-else
-{
-    Console.WriteLine(nombre);
-}
+﻿//string nombre = "Coca";
+//if (string.IsNullOrWhiteSpace(nombre)) 
+//{
+//    Console.WriteLine("string vacio");
+//}
+//else
+//{
+//    Console.WriteLine(nombre);
+//}
+
+int numero = 9;
+string numeroConCero = "99";
+numeroConCero = numeroConCero.PadLeft(3, '$');
+Console.WriteLine(numeroConCero);
+
